c44bd719d819e3ce480285b6d143550a35349a15
edwin_goh88 <gohbiscuit@gmail.com>
2024-05-04 17:13:05 +0800

    - cleanup unused files

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,3 @@
-import { enableProdMode } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/components/app.component';
-import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-
-// Stand alone app
-// bootstrapApplication(AppComponent {
-//   providers: [provideHttpClient(), provideRouter(routes)],
-// }).catch((err) => console.error(err));
-
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
